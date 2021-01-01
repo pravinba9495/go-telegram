effc75518b6385c8e60744d19e4294d9b323509d
Praveen Ravichandran <me@pravinba9495.dev>
2020-12-31 19:00:00 -0500

    Return response body instead of raw response

--- telegram/GetFile.go
+++ telegram/GetFile.go
@@ -1,8 +1,19 @@
 package telegram
 
-import "net/http"
+import (
+	"errors"
+	"io"
+	"net/http"
+)
 
 // GetFile retrieves basic info about a file and prepares it for downloading
-func GetFile(botToken string, fileId string) (*http.Response, error) {
-	return http.Get("https://api.telegram.org/" + botToken + "/getFile?file_id=" + fileId)
+func GetFile(botToken string, fileId string) (io.ReadCloser, error) {
+	response, err := http.Get("https://api.telegram.org/" + botToken + "/getFile?file_id=" + fileId)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, errors.New("getFile request returned: " + response.Status)
+	}
+	return response.Body, nil
 }
