d53b0890488dac2f976866bf3012a1ac72c5282e
Praveen Ravichandran <me@pravinba9495.dev>
2021-07-01 11:55:25 -0400

    Update Chat.go

--- Chat.go
+++ Chat.go
@@ -4,6 +4,6 @@ package telegram
 type Chat struct {
 	ID        uint64 `json:"id"`
 	Username  string `json:"username,omitempty"`
-	FirstName string `json:"firstName,omitempty"`
-	LastName  string `json:"lastName,omitempty"`
+	FirstName string `json:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty"`
 }
